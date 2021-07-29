9e1b278acab722d1ca00837984e56d5e50c430a1
Iudys <iuriguimaraes@hotmail.com>
2021-07-29 12:29:24 -0300

    Alguns cuidados com numeros

---
+++ Fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7/0) // gera um numero infinto
+console.log("10" /2) // ele entennde um valor numerico em ""
+console.log("Show" *2) // ele vai detectar q essa string não é um numero
+console.log(0.1 + 0.7)
+//console.log(10.toString())
+console.log((10.345).toFixed(2))
