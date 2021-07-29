d8bbcd1a90ba4ca89cb8304f9d44fea842ea2606
Iudys <iuriguimaraes@hotmail.com>
2021-07-29 18:03:07 -0300

    Null e Undefined

---
+++ Fundamentos/Null&Undefined.js
@@ -0,0 +1,35 @@
+const a = {name: 'Teste'}
+// = undefined
+console.log(a)
+// {name: 'Teste}
+
+const b = a
+console.log(b)
+
+//  referencia : a const a possui o endereço e qnd atribuo o b, ele passa tbm a receber o msm enderço
+// apotam para o mesmo lugar
+
+b.name = 'Opa'
+
+console.log(a)
+console.log(b)
+
+// Com objetos a passagem é por referencia , com valores primitivos como boolean, numeros ele pega o valor
+//-------------------------------------------------------------------------------------------------------------------------------------
+//null
+
+let valor // não inicializado
+console.log(valor)
+valor = null // ausência de valor
+console.log(valor)
+//console.log(valor.toString()) // Error!
+
+const produto = {}
+console.log(produto.preco) // vai dar undefined pq não esta referenciado o valor do produto
+console.log(produto)
+produto.preco = 3.50
+console.log(produto)
+
+produto.preco =null // sem preço
+console.log(!!produto.preco)
+console.log(produto)
