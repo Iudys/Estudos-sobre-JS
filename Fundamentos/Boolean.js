3a33de1029f47137e9cc7a12732a904de12d5227
Iudys <iuriguimaraes@hotmail.com>
2021-07-29 16:18:59 -0300

    Boolean

---
+++ Fundamentos/Boolean.js
@@ -0,0 +1,29 @@
+let isAtivo = false
+console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log(!!isAtivo) // ! nega !! passa a ser verdadeiro
+
+console.log('os verdadeiros')
+console.log(!!3)
+console.log(!!-1)
+console.log(!!' ')
+console.log(!!'texto')
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!(isAtivo =  true))
+
+console.log('os Falsos')
+console.log((!!0))
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isAtivo = false))
+
+let nome = 'Lucas'
+console.log(nome || 'desconhecido')
