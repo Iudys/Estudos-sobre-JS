b5cd3b23a8f2960a00f3e001980cbac200b22618
Iudys <iuriguimaraes@hotmail.com>
2021-07-30 11:56:41 -0300

    ParNomeValor

---
+++ Fundamentos/ParNomeValor.js
@@ -0,0 +1,23 @@
+// Par nome/valor
+
+const saudacao = " Opa"; //contexto léxico 1
+
+function exec() {
+    const saudacao = "Falaaa"; //contexto léxico 2
+    return saudacao;
+}
+
+// objetivos são grupos aninhados de pares nome/valor
+
+const client = {
+    nome: "Pedro",
+    idade: 32,
+    peso: 90,
+    endereço: {
+        logradouro: "Rua muito legal",
+        numero: 123,
+    },
+};
+
+console.log(saudacao);
+console.log(exec());
