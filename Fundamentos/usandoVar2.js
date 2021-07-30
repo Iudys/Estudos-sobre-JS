3a61d433d7922098bd27c8c711c12ccac3969f26
Iudys <iuriguimaraes@hotmail.com>
2021-07-30 10:53:32 -0300

    Declaração de variaveis com var2

---
+++ Fundamentos/usandoVar2.js
@@ -0,0 +1,8 @@
+var numero = 1;
+{
+    var numero = 2;
+    console.log("dentro =", numero);
+}
+console.log("fora = ", numero);
+
+// vc consegue declarar duas variaveis com Var, mas o msm não acontece qnd usamos let
