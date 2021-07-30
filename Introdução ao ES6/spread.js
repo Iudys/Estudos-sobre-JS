d1249e366f1de2e42787bcee17548c3bddc7d454
Iudys <iuriguimaraes@hotmail.com>
2021-07-30 15:35:59 -0300

    Spread e Estuturas Condicionais

---
+++ Introdução ao ES6/spread.js
@@ -0,0 +1,9 @@
+// Spread ...
+
+var partes = ["ombro", "joelhos"];
+var musica = ["cabeca", ...partes, "e", "pés"]; // msm coisa
+// var musica = ["cabeca",'ombro', 'joelhos'  "e", "pés"]; // msm coisa , serve para facilitar a concatenação
+
+function fn(x, y, z) {}
+var args = [0, 1, 2];
+fn(...arg);
