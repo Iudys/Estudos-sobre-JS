5bddc221dcf20180e360a76b44fb72e2a51f55df
Iudys <iuriguimaraes@hotmail.com>
2021-07-30 11:13:21 -0300

    Usando Let1

--- Fundamentos/UsandoLet1.js
+++ Fundamentos/UsandoLet1.js
@@ -4,3 +4,5 @@ let numero = 1;
     console.log("dentro =", numero);
 }
 console.log("fora =", numero);
+
+// palavras reservadas com a palavra var tem escopo global e tem escopo de função e palavras definidas com a palavra let tem escopo global, de função e de bloco
